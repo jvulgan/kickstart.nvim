6cafabf21d456557ba869b199a721ff7b4e2b684
Jakub Vulgan <jvulgan@redhat.com>
2026-03-23 10:47:39 +0100

    Add harpoon configuration

--- init.lua
+++ init.lua
@@ -898,6 +898,73 @@ require('lazy').setup({
       })
     end,
   },
+  -- Harpoon plugin configuration
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    lazy = false,
+    requires = { 'nvim-lua/plenary.nvim' }, -- if harpoon requires this
+    config = function()
+      require('harpoon').setup {}
+
+      local function toggle_telescope_with_harpoon(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = require('telescope.config').values.file_previewer {},
+            sorter = require('telescope.config').values.generic_sorter {},
+          })
+          :find()
+      end
+      -- vim.keymap.set('n', '<leader>a', function()
+      --   local harpoon = require 'harpoon'
+      --   toggle_telescope_with_harpoon(harpoon:list())
+      -- end, { desc = 'Open harpoon window' })
+    end,
+    keys = {
+      {
+        '<leader>a',
+        function() require('harpoon'):list():add() end,
+        desc = 'harpoon file',
+      },
+      {
+        '<C-e>',
+        function()
+          local harpoon = require 'harpoon'
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'harpoon quick menu',
+      },
+      {
+        '<C-h>',
+        function() require('harpoon'):list():select(1) end,
+        desc = 'harpoon to file 1',
+      },
+      {
+        '<C-t>',
+        function() require('harpoon'):list():select(2) end,
+        desc = 'harpoon to file 2',
+      },
+      {
+        '<C-n>',
+        function() require('harpoon'):list():select(3) end,
+        desc = 'harpoon to file 3',
+      },
+      {
+        '<C-s>',
+        function() require('harpoon'):list():select(4) end,
+        desc = 'harpoon to file 4',
+      },
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
